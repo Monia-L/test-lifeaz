9d3be8a33e7388c8ee7e88b3515d81fd22b8dd75
Monia-L <monia.laoufi@gmail.com>
2019-08-07 16:24:58 +0200

    [code] I gathered everything in the APP.JS file and simplified the route on home.page.ts

--- back/app.js
+++ back/app.js
@@ -4,7 +4,8 @@ const morgan = require("morgan");
 const bodyParser = require('body-parser')
 const cors = require('cors')
 // Getting all routes
-const routes = require("./routes/get_data")
+//const routes = require("./routes/get_data")
+const connection = require('./connection/db')
 
 // Instantiate server
 const app = express();
@@ -14,23 +15,32 @@ app.use(morgan("dev"));
 app.use(cors());
 
 // Body Parser configuration
-app.use(bodyParser.json({limit: '10mb', extended: true}))
-app.use(bodyParser.urlencoded({limit: '10mb', extended: true}))
+app.use(bodyParser.json({ limit: '10mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }))
 
 // Routes
-app.use("/temoignages", (req, res, next) =>{
+// app.use("/", (req, res, next) =>{
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, Origin, Authorization, Accept, Client-Security-Token, Accept-Encoding");
+//     res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
+//     res.header("Access-Control-Max-Age", "1000")
+
+//     next();
+//  }, routes)
+
+app.get('/', (req, res, ) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, Origin, Authorization, Accept, Client-Security-Token, Accept-Encoding");
     res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
     res.header("Access-Control-Max-Age", "1000")
-  
-    next();
- }, routes)
+    connection.query('SELECT * FROM temoignages ORDER BY date DESC', (err, results) => {
+        if (err)
+            // Si une erreur est survenue, alors on informe l'utilisateur de l'erreur
+            throw res.status(500).send('Erreur lors de la récupération des employés');
+        // Si tout s'est bien passé, on envoie le résultat de la requête SQL en tant que JSON.
+        return res.status(200).json(results);
 
-// app.get('/', (req, res,) => {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//     res.send('Ok je suis dans /temoignages');
-// });
+    })
+});
 
 module.exports = app;
--- back/routes/get_data.js
+++ back/routes/get_data.js
@@ -1,18 +1,18 @@
-const express = require("express")
-const connection = require('../connection/db')
+// const express = require("express")
+// const connection = require('../connection/db')
 
-// Router
-const router = express.Router();
+// // Router
+// const router = express.Router();
 
-router.get("/", (req, res) => {
-    connection.query('SELECT * FROM temoignages ORDER BY date DESC', (err, results) => {
-      if (err) 
-        // Si une erreur est survenue, alors on informe l'utilisateur de l'erreur
-        throw res.status(500).send('Erreur lors de la récupération des employés');
-       // Si tout s'est bien passé, on envoie le résultat de la requête SQL en tant que JSON.
-       return res.status(200).json(results);
+// router.get("/", (req, res) => {
+//     connection.query('SELECT * FROM temoignages ORDER BY date DESC', (err, results) => {
+//       if (err) 
+//         // Si une erreur est survenue, alors on informe l'utilisateur de l'erreur
+//         throw res.status(500).send('Erreur lors de la récupération des employés');
+//        // Si tout s'est bien passé, on envoie le résultat de la requête SQL en tant que JSON.
+//        return res.status(200).json(results);
        
-      })
-  })
+//       })
+//   })
 
-module.exports = router;
+// module.exports = router;
